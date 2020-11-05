444eaf5ad85b077864196d1c6835696cf1dc2556
unknown <leonardoz@allegheny.edu>
2020-11-05 15:16:15 -0500

    testing for account service

---
+++ BankingManagement/src/test/java/BankingManagment/Service/test/AccountServiceImplTest.java
@@ -0,0 +1,63 @@
+package BankingManagment.Service.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import BankingManagement.pojos.Account;
+import BankingManagement.pojos.Banking;
+import BankingManagement.service.AccountServiceImpl;
+import BankingManagement.service.CustomCacheServiceImpl;
+
+public class AccountServiceImplTest {
+	
+	private static CustomCacheServiceImpl<Account> cacheService;
+	private static AccountServiceImpl accountService = new AccountServiceImpl();
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		cacheService = new CustomCacheServiceImpl<Account>();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreateSimpleAccount() {
+		String name = "Zach";
+		String password = "password";
+		accountService.createAccount(name, password);
+		assertNotNull("Cache shouldn't be empty" , cacheService.retrieveAllItems());
+	}
+	
+	@Test
+	public void testAuthentication() {
+		String name = "Zach";
+		String password = "password";
+		accountService.createAccount(name, password);
+		assertNotNull("Authentication should return account", accountService.authenticate(name, password));
+	}
+	
+	@Test
+	public void testRemoveAccount() {
+		String name = "Zach";
+		String password = "password";
+		Account testAccount = new Account(name,password,0);
+		cacheService.addToCache(testAccount);
+		accountService.removeAccount(name, password);
+		assertNull("Cache should be empty",cacheService.retrieveItem(testAccount));
+	}
+}
